095d9e43886b790d0f1400d1fd98a65744650d3b
Rohit91Shelar <rohit_shelar@numeroeins.com>
2017-11-10 20:03:41 +0530

    Commit Upto dashboard

--- app/src/main/java/com/numeroeins/uniflea/activities/DashboardActivity.java
+++ app/src/main/java/com/numeroeins/uniflea/activities/DashboardActivity.java
@@ -23,10 +23,13 @@ public class DashboardActivity extends AppCompatActivity {
     RecyclerView recycleviewForProductList;
     @BindView(R.id.recycleviewForServiceList)
     RecyclerView recycleviewForServiceList;
-    @BindView(R.id.moreProdcuts)TextView moreProdcuts;
-    @BindView(R.id.moreServices)TextView moreServices;
+    @BindView(R.id.moreProdcuts)
+    TextView moreProdcuts;
+    @BindView(R.id.moreServices)
+    TextView moreServices;
 
-    ArrayList<Product> list=new ArrayList<>();
+    ArrayList<Product> list = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,25 +44,25 @@ public class DashboardActivity extends AppCompatActivity {
     private void showProductList() {
 
         createDummyData();
-        ProductListAdapter productListAdapter=new ProductListAdapter(this,this.list);
-        ProductListAdapter serviceListAdapter=new ProductListAdapter(this,this.list);
-        recycleviewForProductList.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+        ProductListAdapter productListAdapter = new ProductListAdapter(this, this.list);
+        ProductListAdapter serviceListAdapter = new ProductListAdapter(this, this.list);
+        recycleviewForProductList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         recycleviewForProductList.setAdapter(productListAdapter);
 
-        recycleviewForServiceList.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+        recycleviewForServiceList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         recycleviewForServiceList.setAdapter(serviceListAdapter);
 
     }
 
     private void createDummyData() {
-        this.list.add(new Product(R.drawable.image1,"Product 1","100","Pune",false));
-        this.list.add(new Product(R.drawable.image2,"Product 2","200","Pune",true));
-        this.list.add(new Product(R.drawable.img3,"Product 3","300","Pune",false));
-        this.list.add(new Product(R.drawable.img4,"Product 4","400","Pune",false));
-        this.list.add(new Product(R.drawable.img5,"Product 5","500","Pune",false));
-        this.list.add(new Product(R.drawable.img6,"Product 6","600","Pune",false));
-        this.list.add(new Product(R.drawable.img7,"Product 7","700","Pune",false));
-        this.list.add(new Product(R.drawable.noimage,"Product 8","800","Pune",false));
+        this.list.add(new Product(R.drawable.image1, "Product 1", "100", "Pune", false));
+        this.list.add(new Product(R.drawable.image2, "Product 2", "200", "Pune", true));
+        this.list.add(new Product(R.drawable.img3, "Product 3", "300", "Pune", false));
+        this.list.add(new Product(R.drawable.img4, "Product 4", "400", "Pune", false));
+        this.list.add(new Product(R.drawable.img5, "Product 5", "500", "Pune", false));
+        this.list.add(new Product(R.drawable.img6, "Product 6", "600", "Pune", false));
+        this.list.add(new Product(R.drawable.img7, "Product 7", "700", "Pune", false));
+        this.list.add(new Product(R.drawable.noimage, "Product 8", "800", "Pune", false));
 
     }
 }
